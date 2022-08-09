b384d4abf71dd1d86e61823672a0beb773b7d572
BirhanuGebisa <birhanugebisa>
2022-08-09 09:48:18 +0300

    fix bug

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -23,6 +23,12 @@ class TweetDfExtractor:
     def __init__(self, tweets_list):
         
         self.tweets_list = tweets_list
+    
+    def find_statuses_count(self) -> list:
+        statuses_count = []
+        for tweet in self.tweets_list:
+            statuses_count.append(tweet['user']['statuses_count'])
+        return statuses_count
   
     def find_full_text(self)->list:
         text = []
@@ -50,6 +56,21 @@ class TweetDfExtractor:
         source = [["source"] for x in self.tweets_list]
 
         return source
+
+    def is_sensitive(self)->list:
+        is_sensitive=[]
+        for tweet in self.tweets_list:
+            if 'possibly_sensitive' in tweet.keys():
+                is_sensitive.append(tweet['possibly_sensitive'])
+            else: is_sensitive.append(None)
+
+        return is_sensitive
+        """try:
+            is_sensitive = [x['possibly_sensitive'] for x in self.tweets_list]
+        except KeyError:
+            is_sensitive = None
+        
+        return is_sensitive """  
     #find favourite count
     def find_favourite_count(self)->list:
         favourite_count = [x.get("retweeted_status", {}).get("favourite_count", 0) for x in self.tweets_list]
@@ -65,6 +86,30 @@ class TweetDfExtractor:
                 retweet_count.append(None)
 
         return retweet_count     
+    def find_hashtags(self) -> list:
+        hashtags = []
+        for tweet in self.tweets_list:
+            try:
+                hashtags.append(tweet['entities']['hashtags'][0]['full_text'])
+            except KeyError:
+                hashtags.append(None)
+            except IndexError:
+                hashtags.append(None)
+        return hashtags
+    
+    def find_mentions(self) -> list:
+        mentions = []
+        for hs in self.tweets_list:
+            mentions.append(", ".join(
+                [mention['screen_name'] for mention in hs['entities']['user_mentions']]))
+        return mentions
+    def find_location(self)->list:
+        try:
+            location = self.tweets_list['user']['location']
+        except TypeError:
+            location = ''
+        
+        return location
     
     def find_lang(self)->list:
         lang = [x['lang'] for x in self.tweets_list]
@@ -74,7 +119,7 @@ class TweetDfExtractor:
     def get_tweet_df(self, save=False)->pd.DataFrame:
         """required column to be generated you should be creative and add more features"""
         
-        columns = ['created_at', 'source', 'full_text','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count']
+        columns = ['created_at', 'source', 'full_text','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count','possibly_sensitive']
         
         created_at = self.find_created_time()
         source = self.find_source()
@@ -83,8 +128,11 @@ class TweetDfExtractor:
         lang = self.find_lang()
         fav_count = self.find_favourite_count()
         retweet_count = self.find_retweet_count()
-        #possibly_sensitive  = self.is_sensitive()
-        data = zip(created_at, source, full_text, polarity, subjectivity, lang, fav_count, retweet_count)
+        sensitive  = self.is_sensitive()
+        hashtags = self.find_hashtags()
+        mentions = self.find_mentions()
+        location = self.find_location()
+        data = zip(created_at, source, full_text, polarity, subjectivity, lang, fav_count, retweet_count,sensitive, hashtags, mentions, location)
         df = pd.DataFrame(data=data, columns=columns)
 
         if save:
@@ -96,7 +144,7 @@ class TweetDfExtractor:
 #main code      
 if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
-    columns = ['created_at', 'source', 'full_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count','possibly_sensitive']
+    columns = ['created_at', 'source', 'full_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count','possibly_sensitive','hashtags', 'user_mentions', 'location']
     _, tweet_list = read_json("data/global_twitter_data.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df(save=True) 
