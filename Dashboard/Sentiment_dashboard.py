5bbaff5df8824457ad785143b6cac31128687159
BirhanuGebisa <birhanugebisa>
2022-08-12 04:13:33 +0300

    sentiment dahsboard using streamlint

--- Dashboard/Sentiment_dashboard.py
+++ Dashboard/Sentiment_dashboard.py
@@ -1,4 +1,12 @@
 #python library
 import pandas as pd
 import numpy as np
-import streamlint as st
+import streamlit as st
+
+
+st.write("""
+#sentimentall Analysis of Twitter dataset
+# using Streamlit
+""")
+tweet_df=pd.read_csv("../data/clean_processed_tweet.csv")
+st.line_chart(tweet_df)
