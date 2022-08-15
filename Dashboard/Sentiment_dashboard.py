d4c1de81ced914a8e7e5103de18c7b149ccd9383
Birhanu G. Muleta <birhanugebisa@gmail.com>
2022-08-15 17:44:51 +0300

    Update Sentiment_dashboard.py

--- Dashboard/Sentiment_dashboard.py
+++ Dashboard/Sentiment_dashboard.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 import altair as alt
-from wordcloud import WordCloud
+#from wordcloud import WordCloud
 import plotly.express as px
 #import sys
 #sys.path.insert(0, 'Sql/Database_Table.py')
